dff2815a7de667f3e9bf200b7af6dd39ea2f0771
angel perez <angel.per.cho@gmail.com>
2019-11-23 16:36:55 -0300

    main view creation

---
+++ src/components/home.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="hello">
+    <img class="img-logo" src="../assets/Orange_Rycom_Logo.png"/>
+    <div class="container-cam">
+        <div class="background-cam"></div>
+    </div>
+    <div class="btn-return"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'home',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+    .img-logo {
+        width: 20%;
+        margin: 0 auto;
+        margin-bottom: 50px;
+    }
+    .container-cam {
+        background-color: black;
+        width: 50%;
+        height: 400px;
+        margin: 0 auto;
+        box-shadow: 0 0 7px 0 #676767;
+    }
+    .background-cam {
+        background-image: url(../assets/Watermark2.png);
+        width: 100%;
+        height: 100%;
+        z-index: 1;
+        background-size: 101% 100%;
+        background-repeat: no-repeat;
+    }
+    .btn-return {
+        background-image: url(../assets/Main_Menu_Button.png);
+        width: 15%;
+        height: 56px;
+        background-size: 100%;
+        background-repeat: no-repeat;
+        margin: 0 auto;
+        margin-top: 40px;
+        cursor: pointer;
+    }
+</style>
