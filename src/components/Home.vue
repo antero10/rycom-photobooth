652addd92400f33abf643b5fece894b958860bc4
angel perez <angel.per.cho@gmail.com>
2019-11-27 11:16:14 -0300

    fix size canvas and add two buttons

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -3,13 +3,17 @@
     <div class="hello">
       <img class="img-logo" src="../assets/Orange_Rycom_Logo.png"/>
       <div class="container-cam">
-          <video ref="video" autoplay="true" id="videoElement"/>
+          <video v-if="canvas" ref="video" autoplay="true" id="videoElement"/>
+          <canvas v-if="!canvas" ref="canvas" id="canvas" width="640" height="480"></canvas>
           <div class="background-cam">
             
           </div>
       </div>
-      <button class="btn-return" v-on:click="capture()"></button>
-      <canvas ref="canvas" id="canvas" width="640" height="480" style="display:none;"></canvas>
+      <button class="btn-return m-top-30" v-on:click="capture()"></button> <br>
+      <button class="btn-return btn-img1" v-on:click="capture()"></button> <br>
+      <button class="btn-return btn-img2" v-on:click="capture()"></button> <br>
+
+      <!--<button class="input-form button-form">text</button>-->
     </div>
 </div>
 </template>
@@ -50,9 +54,17 @@ export default {
     video#videoElement {
       position: absolute;
       top: 25px;
-      left: 28px;
+      left: 8px;
       width: 798px;
-      height: 578px;
+      height: 549px;
+      z-index: 0;
+    }
+    canvas#canvas {
+      position: absolute;
+      top: 25px;
+      left: 8px;
+      width: 798px;
+      height: 549px;
       z-index: 0;
     }
     .img-logo {
@@ -62,8 +74,8 @@ export default {
     }
     .container-cam {
       background-color: black;
-      width: 1066px;
-      height: 626px;
+      width: 1039px;
+      height: 585px;
       margin: 0 auto;
       box-shadow: 0 0 7px 0 #676767;
       position: relative;
@@ -84,11 +96,40 @@ export default {
         background-size: 100%;
         background-repeat: no-repeat;
         margin: 0 auto;
-        margin-top: 40px;
         cursor: pointer;
         border: none;
+        background-color: transparent;
+    }
+    .m-top-30 {
+      margin-top: 30px;
+    }
+    .btn-img1 {
+      background-image: url(../assets/Main_Menu_Button.png) !important;
+    }
+    .btn-img2 {
+      background-image: url(../assets/Main_Menu_Button.png) !important;
     }
     .btn-return:focus {
-        outline: none;
+      outline: none;
+    }
+    .input-form {
+        background: linear-gradient(to right, rgba(226,72,49,1) 0%, rgba(233,117,55,1) 42%, rgba(233,117,55,1) 58%, rgba(226,72,49,1) 100%);
+        border-radius: 38px;
+        color: white;
+        text-align: center;
+        width: 100%;
+        height: 75px;
+        border: none;
+        margin: 6px 0px;
+        font-size: 24px;
+    }
+    .input-form::placeholder {
+        color: white;
+    }
+    .button-form {
+        width: 35%;
+        box-shadow: 1px 1px 4px 0 #000000c7;
+        margin-top: -22px;
+        margin-bottom: 0;
     }
 </style>
